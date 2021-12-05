5c7ad29a604f116305a194c4095e222feb198b67
hkngkblt <47926439+hkngkblt@users.noreply.github.com>
2021-12-06 02:53:28 +0300

    remove gitignore files

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_usd_price():
         'data']['825']['quote']['TRY']['price']  # float price
     return data
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # if user login => redirect login
     # else redirect => homepage
