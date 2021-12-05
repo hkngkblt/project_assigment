aad06900c41a69c08cbebaa47d21f193e0771e65
hkngkblt <47926439+hkngkblt@users.noreply.github.com>
2021-12-06 02:42:46 +0300

    remove gitignore files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""
+
 import copy
 import json
 from requests import Session
@@ -6,18 +6,7 @@ from flask import Flask, request, abort, Response
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "helloworld"
-"""
-import copy
-from flask import Flask
-from os import path
-from flask import Flask, abort, render_template, request, redirect, url_for, session, g, send_from_directory, Response, send_file, make_response
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "helloworld"
-
-@app.route('/', methods=['GET', 'POST'])
-def particle():
-    abort(Response("empty page",401))
 
 # get usdt price with Coinmarketcap API
 def get_usd_price():
@@ -37,24 +26,22 @@ def get_usd_price():
         'data']['825']['quote']['TRY']['price']  # float price
     return data
 
-"""
 @app.route('/', methods=['GET'])
 def index():
     # if user login => redirect login
     # else redirect => homepage
-    abort(Response(401))
+    abort(Response("empty page",401))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # login and redirect homepage
-    abort(Response(401))
-"""
+    abort(Response("empty page",401))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # register and redirect index
-    abort(Response(401))
+    abort(Response("empty page",401))
 
 
 users = [
